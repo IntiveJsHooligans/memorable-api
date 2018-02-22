e6c00031655d26a190a156ae2e978fbdad2e2348
mariuszkalinskiifm <maniek.kalinski@gmail.com>
2018-02-22 23:03:59 +0100

    JSHOOLSGUILD-15 create task crud
    created new eslint rules
    
    created crud for tasks
    
    add seed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -7,6 +7,29 @@ function feed(parent, args, context, info) {
   return context.db.query.links({ first, skip, where }, info);
 }
 
+function tasks(parent, args, context, info) {
+  const { filter, first, skip } = args; // destructure input arguments
+  const where = filter
+    ? { OR: [
+      { title_contains: filter },
+      { description_contains: filter },
+    ] }
+    : {};
+
+  return context.db.query.tasks({ first, skip, where }, info);
+}
+
+function taskStatuses(parent, args, context, info) {
+  const { filter, first, skip } = args; // destructure input arguments
+  const where = filter
+    ? { OR: [{ url_contains: filter }] }
+    : {};
+
+  return context.db.query.taskStatuses({ first, skip, where }, info);
+}
+
 module.exports = {
   feed,
+  tasks,
+  taskStatuses,
 };
